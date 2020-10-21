3117fef0a5892c1e73ca639e8c9f1cccb718bcbc
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-21 00:51:51 -0700

    uh..

--- control.lua
+++ control.lua
@@ -1,1 +1,19 @@
 
+script.on_load()
+  commands.add_command("cutscene-creator", help, play_cutscene(name, tick, player_index, parameter))
+end
+
+function play_cutscene(name, tick, player_index, parameter)
+  if name == "cutscene-creator" then
+    game.players[player_index].set_controller{
+      type = defines.controllers.cutscene,
+      -- character = game.players[player_index],
+      waypoints = create_waypoints(parameter),
+      -- final_transition_time = final_transition_time(parameter)
+    }
+  end
+end
+
+function create_waypoints(parameter)
+  local waypoints = game.json_to_table(parameter)
+  
