dcd2a964799a653afa0e21a6006137fd28fde2e2
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-23 09:22:36 -0700

    create_waypoints might work now??
    big huge thanks to Choumiko and ルシファー せんぱい [LuziferSenpai] on discord for tons of patience and help with this one!

--- control.lua
+++ control.lua
@@ -8,51 +8,73 @@ function play_cutscene(name, tick, player_index, parameter)
     game.players[player_index].set_controller{
       type = defines.controllers.cutscene,
       -- character = game.players[player_index],
-      waypoints = create_waypoints(parameter),
+      waypoints = create_waypoints_simple(parameter),
       -- final_transition_time = final_transition_time(parameter)
     }
   end
 end
 
--- example parameter = "[gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3"
-function create_waypoints(parameter)
+function create_waypoints_simple(parameter)
+  local parameter = "[gps=51,37,nauvis][gps=51,38,nauvis][gps=53,38,nauvis]"
   local waypoints = {}
-  local parameter = parameter:gsub("%[","{")
-  local parameter = parameter:gsub("%]","}")
-  local parameter = parameter:gsub("gps=","position={")
-  local parameter = parameter:gsub("%stt",", transition_time=")
-  local parameter = parameter:gsub("%swt",", time_to_wait=")
-  local parameter = parameter:gsub("%sz",", zoom=")
-  local parameter = parameter:gsub("%s%{","}, {")
+  local tt, tw, z = "transition_time=300", "time_to_wait=30", "zoom=3"
+  parameter = parameter:gsub("%s*",""):gsub("%[","{"):gsub("%]","}"):gsub("gps=","position={"):gsub("%}%{","}}, {")
+  parameter = parameter.."}"
+  parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
+  local proc, errmsg = load('local waypoints={'..parameter..'} return waypoints')
+  if proc then
+  local status, result = pcall(proc)
+      if status then
+        waypoints = result
+        return waypoints
+      else
+          print("pcall failed: "..result)
+      end
+  else
+      print("load failed: "..errmsg)
+  end
 end
 
-function create_waypoints_simple(parameter)
-  local waypoints = {}
-  local tt = "transition_time=300"
-  local tw = "time_to_wait=30"
-  local z = "zoom=3"
-  local parameter = parameter:gsub("%[","{")
-  local parameter = parameter:gsub("%]","}")
-  local parameter = parameter:gsub("gps=","position={")
-  local parameter = parameter:gsub("%}%{","}} {")
-  local parameter = parameter.."}"
-  local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
-  for i in parameter:gmatch("%S+") do
-   table.insert(waypoints, i)
-  end
-  return waypoints
-end
+-- -- example parameter = "[gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3"
+-- function create_waypoints(parameter)
+--   local waypoints = {}
+--   local parameter = parameter:gsub("%[","{")
+--   local parameter = parameter:gsub("%]","}")
+--   local parameter = parameter:gsub("gps=","position={")
+--   local parameter = parameter:gsub("%stt",", transition_time=")
+--   local parameter = parameter:gsub("%swt",", time_to_wait=")
+--   local parameter = parameter:gsub("%sz",", zoom=")
+--   local parameter = parameter:gsub("%s%{","}, {")
+-- end
 
-function create_waypoints_simple(parameter)
-  local waypoints = {}
-  local tt = "transition_time=300"
-  local tw = "time_to_wait=30"
-  local z = "zoom=3"
-  local parameter = parameter:gsub("%s*",""):gsub("%[","{"):gsub("%]","}"):gsub("gps=","position={"):gsub("%}%{","}} {")
-  local parameter = parameter.."}"
-  local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
-  for i in parameter:gmatch("%S+") do
-   table.insert(waypoints, i)
-  end
-  return waypoints
-end
+-- function create_waypoints_simple(parameter)
+--   local waypoints = {}
+--   local tt = "transition_time=300"
+--   local tw = "time_to_wait=30"
+--   local z = "zoom=3"
+--   local parameter = parameter:gsub("%[","{")
+--   local parameter = parameter:gsub("%]","}")
+--   local parameter = parameter:gsub("gps=","position={")
+--   local parameter = parameter:gsub("%}%{","}} {")
+--   local parameter = parameter.."}"
+--   local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
+--   for i in parameter:gmatch("%S+") do
+--    table.insert(waypoints, i)
+--   end
+--   return waypoints
+-- end
+
+-- function create_waypoints_simple(parameter)
+--   local waypoints = {}
+--   local tt = "transition_time=300"
+--   local tw = "time_to_wait=30"
+--   local z = "zoom=3"
+--   local parameter = parameter:gsub("%s*",""):gsub("%[","{"):gsub("%]","}"):gsub("gps=","position={"):gsub("%}%{","}} {")
+--   local parameter = parameter.."}"
+--   local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
+--   for i in parameter:gmatch("%S+") do
+--    table.insert(waypoints, i)
+--   end
+--   return waypoints
+-- end
+
