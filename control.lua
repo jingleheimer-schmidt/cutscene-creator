8e979f219d8ee35c7fed0ac741ca088ffe8cefec
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-23 23:26:37 -0700

    added pcall so game doesn't crash for bad arguments

--- control.lua
+++ control.lua
@@ -1,27 +1,51 @@
 
 script.on_init(function()
-  commands.add_command("cutscene", "shift-click on map to create waypoints", play_cutscene)
-  commands.add_command("cc", "/c cc tt<transition time (ticks)> wt<waiting time (ticks)> z<zoom>", play_cutscene)
+  commands.add_command("cutscene", "shift-click on map to create waypoints, then press enter to play cutscene", play_cutscene)
+  commands.add_command("cc", "/cc tt<transition time (ticks)> wt<waiting time (ticks)> z<zoom at position>", play_cutscene)
 end)
+-- script.on_configuration_changed(function()
+--   commands.add_command("cutscene", "shift-click on map to create waypoints, then press enter to play cutscene", play_cutscene)
+--   commands.add_command("cc", "/cc tt<transition time (ticks)> wt<waiting time (ticks)> z<zoom at position>", play_cutscene)
+-- end)
 
 function play_cutscene(command)
   local player_index = command.player_index
   local parameter = command.parameter
   local name = command.name
   if name == "cutscene" then
-    game.players[player_index].set_controller{
-      type = defines.controllers.cutscene,
-      waypoints = create_waypoints_simple(parameter, player_index)
-      -- final_transition_time = final_transition_time(parameter)
-    }
+    local status, result = pcall(create_cutscene, parameter, player_index)
+    if status then
+      create_cutscene(parameter, player_index)
+    else
+      game.print("failed to create cutscene: invalid waypoints")
+    end
   end
   if name == "cc" then
-    game.players[player_index].set_controller{
-      type = defines.controllers.cutscene,
-      waypoints = create_waypoints_custom(parameter)
-      -- final_transition_time = final_transition_time(parameter)
-    }
+    local status, result = pcall(create_cutscene, parameter, player_index)
+    if status then
+      create_cutscene_custom(parameter, player_index)
+    else
+      game.print("failed to create cutscene: invalid waypoints")
+    end
   end
+end
+
+function create_cutscene(parameter, player_index)
+  game.players[player_index].set_controller{
+    type = defines.controllers.cutscene,
+    waypoints = create_waypoints_simple(parameter, player_index),
+    start_position = game.players[player_index].position
+    -- final_transition_time = final_transition_time(parameter)
+  }
+end
+
+function create_cutscene_custom(parameter, player_index)
+  game.players[player_index].set_controller{
+    type = defines.controllers.cutscene,
+    waypoints = create_waypoints_custom(parameter),
+    start_position = game.players[player_index].position
+    -- final_transition_time = final_transition_time(parameter)
+  }
 end
 
 function create_waypoints_simple(parameter, player_index)
