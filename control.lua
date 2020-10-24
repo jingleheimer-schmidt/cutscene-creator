846aac16545d3390e1f6494b8dd6d3f7626c9068
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-23 17:25:37 -0700

    removed old broken code, updated help, fixed print

--- control.lua
+++ control.lua
@@ -1,23 +1,21 @@
 
 script.on_load()
-  commands.add_command("cutscene", help, play_cutscene(name, tick, player_index, parameter))
-  commands.add_command("cc", help, play_cutscene(name, tick, player_index, parameter))
+  commands.add_command("cutscene", "shift-click on map to create waypoints", play_cutscene(name, tick, player_index, parameter))
+  commands.add_command("cc", "/c cc tt<transition time (ticks)> wt<waiting time (ticks)> z<zoom>", play_cutscene(name, tick, player_index, parameter))
 end
 
 function play_cutscene(name, tick, player_index, parameter)
   if name == "cutscene" then
     game.players[player_index].set_controller{
       type = defines.controllers.cutscene,
-      -- character = game.players[player_index],
-      waypoints = create_waypoints_simple(parameter, player_index),
+      waypoints = create_waypoints_simple(parameter, player_index)
       -- final_transition_time = final_transition_time(parameter)
     }
   end
   if name == "cc" then
     game.players[player_index].set_controller{
       type = defines.controllers.cutscene,
-      -- character = game.players[player_index],
-      waypoints = create_waypoints_custom(parameter),
+      waypoints = create_waypoints_custom(parameter)
       -- final_transition_time = final_transition_time(parameter)
     }
   end
@@ -55,55 +53,11 @@ function create_waypoints_custom(parameter)
   local status, result = pcall(proc)
       if status then
         waypoints = result
-        print(proc)
         return waypoints
       else
-          print("pcall failed: "..result)
+          game.print("pcall failed: "..result)
       end
   else
-      print("load failed: "..errmsg)
+      game.print("load failed: "..errmsg)
   end
-
--- -- example parameter = "[gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3 [gps=0,0] tt300 wt30 z3"
--- function create_waypoints(parameter)
---   local waypoints = {}
---   local parameter = parameter:gsub("%[","{")
---   local parameter = parameter:gsub("%]","}")
---   local parameter = parameter:gsub("gps=","position={")
---   local parameter = parameter:gsub("%stt",", transition_time=")
---   local parameter = parameter:gsub("%swt",", time_to_wait=")
---   local parameter = parameter:gsub("%sz",", zoom=")
---   local parameter = parameter:gsub("%s%{","}, {")
--- end
-
--- function create_waypoints_simple(parameter)
---   local waypoints = {}
---   local tt = "transition_time=300"
---   local tw = "time_to_wait=30"
---   local z = "zoom=3"
---   local parameter = parameter:gsub("%[","{")
---   local parameter = parameter:gsub("%]","}")
---   local parameter = parameter:gsub("gps=","position={")
---   local parameter = parameter:gsub("%}%{","}} {")
---   local parameter = parameter.."}"
---   local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
---   for i in parameter:gmatch("%S+") do
---    table.insert(waypoints, i)
---   end
---   return waypoints
--- end
-
--- function create_waypoints_simple(parameter)
---   local waypoints = {}
---   local tt = "transition_time=300"
---   local tw = "time_to_wait=30"
---   local z = "zoom=3"
---   local parameter = parameter:gsub("%s*",""):gsub("%[","{"):gsub("%]","}"):gsub("gps=","position={"):gsub("%}%{","}} {")
---   local parameter = parameter.."}"
---   local parameter = parameter:gsub("%}%}","},"..tt..","..tw..","..z.."}")
---   for i in parameter:gmatch("%S+") do
---    table.insert(waypoints, i)
---   end
---   return waypoints
--- end
 
