8b18d2676eb53b695797572026c5cdec45a032b6
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-09-24 14:07:51 -0700

    standardize code formatting

--- settings.lua
+++ settings.lua
@@ -1,34 +1,34 @@
 
 local transitionTimeSetting = {
-  type = "int-setting",
-  name = "cc-transition-time",
-  setting_type = "runtime-per-user",
-  minimum_value = 1,
-  default_value = 120,
-  order = "cc-1"
+    type = "int-setting",
+    name = "cc-transition-time",
+    setting_type = "runtime-per-user",
+    minimum_value = 1,
+    default_value = 120,
+    order = "cc-1"
 }
 
 local timeWaitSetting = {
-  type = "int-setting",
-  name = "cc-time-wait",
-  setting_type = "runtime-per-user",
-  minimum_value = 1,
-  default_value = 60,
-  order = "cc-2"
+    type = "int-setting",
+    name = "cc-time-wait",
+    setting_type = "runtime-per-user",
+    minimum_value = 1,
+    default_value = 60,
+    order = "cc-2"
 }
 
 local zoomSetting = {
-  type = "double-setting",
-  name = "cc-zoom",
-  setting_type = "runtime-per-user",
-  minimum_value = .1,
-  default_value = .75,
-  maximum_value = 100,
-  order = "cc-3"
+    type = "double-setting",
+    name = "cc-zoom",
+    setting_type = "runtime-per-user",
+    minimum_value = .1,
+    default_value = .75,
+    maximum_value = 100,
+    order = "cc-3"
 }
 
 data:extend({
-  transitionTimeSetting,
-  timeWaitSetting,
-  zoomSetting
+    transitionTimeSetting,
+    timeWaitSetting,
+    zoomSetting
 })
